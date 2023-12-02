6ed2f2a33b864d2745dc8eda9d95143729492b57
Alyson Tremblay <altrembl@amazon.com>
2023-12-02 17:14:20 -0500

    Updated Try/Catch for Exception Handling

--- src/Tools/LaunchProgram.java
+++ src/Tools/LaunchProgram.java
@@ -13,14 +13,28 @@ public class LaunchProgram {
         if (answer.equalsIgnoreCase(optionOne)) {
             try {
                 myClass.getDeclaredMethod(methodOneName).invoke(null);
-            } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
-                throw new RuntimeException(e);
+            } catch (NoSuchMethodException e) {
+                System.out.println("No such method exception thrown from day one, " +
+                        "check the start method in your day's named class.");
+            } catch (InvocationTargetException e) {
+                System.out.println("Invocation target exception thrown from day one, " +
+                        "check the start method in your day's named class.");
+            } catch (IllegalAccessException e) {
+                System.out.println("Illegal access exception thrown from day one, " +
+                        "check the start method in your day's named class.");
             }
         } else if (answer.equalsIgnoreCase(optionTwo)) {
             try {
                 myClass.getDeclaredMethod(methodTwoName).invoke(null);
-            } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
-                throw new RuntimeException(e);
+            } catch (NoSuchMethodException e) {
+                System.out.println("No such method exception thrown from day two, " +
+                        "check the start method in your day's named class.");
+            } catch (InvocationTargetException e) {
+                System.out.println("Invocation target exception thrown from day two, " +
+                        "check the start method in your day's named class.");
+            } catch (IllegalAccessException e) {
+                System.out.println("Illegal access exception thrown from day two, " +
+                        "check the start method in your day's named class.");
             }
         } else if (answer.equalsIgnoreCase("Exit")) {
             System.exit(0);
